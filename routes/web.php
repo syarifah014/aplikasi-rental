87f24052328aba4121a59d69cfa729d0bdaded97
NamaAnda <aqilahsyarifah014@gmail.com>
2026-03-17 10:29:27 +0700

    tugas

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID: ' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+    
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
